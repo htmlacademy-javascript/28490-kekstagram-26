1850a3eb8d207dfa9cd8f8f2195eb6b8bd64a4a8
Maria <maria4355@gmail.com>
2022-06-18 15:04:49 +0300

    Functions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-const MIN = 50;
-const MAX = 80;
 const COMMENT = 'dmk,lkjhgf ddhl;lkjhgsdfg hjmk,lkjhgfdmk,lkjhgfd';
 const MAX_LENGTH = 140;
+const MIN = 50;
+const MAX = 80;
 
 const checkStringLength = () => COMMENT.length <= MAX_LENGTH;
 
 checkStringLength(COMMENT, MAX_LENGTH);
 
-// eslint-disable-next-line no-shadow
-const getRandomIntInclusive = (MIN, MAX) => {
-  MIN = Math.ceil(MIN);
-  MAX = Math.floor(MAX);
-
-  return Math.floor(Math.random() * (MAX - MIN + 1)) + MIN;
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 getRandomIntInclusive(MIN, MAX);
