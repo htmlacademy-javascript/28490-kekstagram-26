e0a386811f8c754dc24de9e61b044f3917621ddd
Maria <maria4355@gmail.com>
2022-06-28 15:23:48 +0300

    Module4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const ITEMS = [];
-ITEMS.COMMENTS = [
+const items = [];
+const comments = [
   'Всё отлично!',
   'В целом всё неплохо.Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
@@ -7,7 +7,7 @@ ITEMS.COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !'
 ];
-ITEMS.NAMES = [
+const names = [
   'Sasha',
   'Mike',
   'Dani',
@@ -17,6 +17,19 @@ ITEMS.NAMES = [
   'Misha',
   'Osaka'
 ];
+const descriptions = [
+  'All My Loving',
+  'All Together Now',
+  'Drive My Car',
+  'Getting Better',
+  'Hello, Goodbye',
+  'Help!',
+  'Here Comes The Sun',
+  'Hey Jude',
+  'Love Me Do',
+  'Mr Moonlight',
+  'Michelle'
+];
 
 const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
@@ -24,18 +37,34 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+const getComment = (quantity) => {
+  const comment = comments[getRandomIntInclusive(0, 5)];
+  if (quantity === 1) {
+    return comment;
+  } else {
+    let secondSentanceComment = comments[getRandomIntInclusive(0, 5)];
+    while (comment === secondSentanceComment) {
+      secondSentanceComment = comments[getRandomIntInclusive(0, 5)];
+    }
+    return comment.concat(secondSentanceComment);
+  }
+};
+
 for (let i = 1; i < 26; i++) {
-  const AVATAR_NUMBER = getRandomIntInclusive(1, 6);
-  const LIKES = getRandomIntInclusive(15, 200);
-  const COMMENT_NUMBER = getRandomIntInclusive(0, 5);
-  const NAME_NUMBER = getRandomIntInclusive(0, 7);
-  ITEMS[i] = {
-    Name: ITEMS.NAMES[NAME_NUMBER],
-    ID: i,
-    URL: `photos/${i}.jpg`,
-    Avatar: `img / avatar${AVATAR_NUMBER}.svg`,
-    Likes: LIKES,
-    message: ITEMS.COMMENTS[COMMENT_NUMBER]
+  const avatarNumber = getRandomIntInclusive(1, 6);
+  const likes = getRandomIntInclusive(15, 200);
+  const nameNumber = getRandomIntInclusive(0, 7);
+  const descriptionNumber = getRandomIntInclusive(0, 10);
+  items[i] = {
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: descriptions[descriptionNumber],
+    likes: likes,
+    comments: {
+      id: getRandomIntInclusive(1, 100),
+      avatar: `img / avatar${avatarNumber}.svg`,
+      message: getComment(getRandomIntInclusive(1, 2)),
+      name: names[nameNumber],
+    }
   };
 }
-
